c35345c523a345df2f169dbe218aa1cedf37f53d
gojutin <gojutin@gmail.com>
2019-09-25 23:45:51 -0400

    Updated README

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "My awesome Gatsby site~",
+    title: "My awesome Gatsby site",
     description:
       "This is a basic Gatsby app that was created as a demo for the September 2019 Long Island JavaScript Group Meetup.",
   },
